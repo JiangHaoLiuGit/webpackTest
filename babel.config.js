ef8adba1e2484abcbb430df8ff6582750773424e
LiuJiangHao <jianghaoliu@yeah.net>
2025-08-30 02:10:42 +0800

    补充

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     // 智能预设,能编译es6语法
     presets:['@babel/preset-env']
+    // // 智能预设,能编译jsx语法
+    // presets:['@babel/preset-react']
+    // // 智能预设,能编译ts语法
+    // presets:['@babel/preset-typescript']
 }
